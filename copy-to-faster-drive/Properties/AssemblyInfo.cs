3265e07905d2e7614628670b161a766ef75452cc
Zallist <zallist@gmail.com>
2024-03-04 09:36:42 +0000

    Update wrong copyrights

--- copy-to-faster-drive/Properties/AssemblyInfo.cs
+++ copy-to-faster-drive/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CopyToFasterDrive")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
